6c9ead02b4d0f9d0a45c0c9cc2dd7f96b14e4591
steve hanlon <rshanlon@gmail.com>
2019-03-03 23:37:20 -0800

    Initial commit; start HTML board with javascript populating the alphabet keyboard and answer blanks

---
+++ README.md
@@ -0,0 +1,7 @@
+Project: *Hangman*
+
+CodePen: [Code sample in progress here.](https://codepen.io/SixStringsCoder/pen/KEMXLG)
+
+Goal: Recreate a web version of hangman that can handle single words or multiple-word answers.
+
+Extra features:
---
+++ script.js
@@ -0,0 +1,34 @@
+const alphabetKeys = document.querySelector('#alphabet-keyboard');
+const letterblanks = document.querySelector('#letterspaces');
+const alphabet =   ["A", "E", "I", "O", "U", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+
+
+
+const buildAlphabet = (arr) => {
+  arr.map((letter, index) => {
+    let htmlpattern = `<div class="letter" id="${letter}">${letter}</div>`;
+    alphabetKeys.innerHTML += htmlpattern;
+  });
+}
+
+const secretWord = (word) => {
+  let wordArr = word.toUpperCase().split('');
+  console.log(wordArr);
+  wordArr.map((letter) => {
+    let blank = `<div class="word-letter" data-value="${letter}"></div>`;
+    letterblanks.innerHTML += blank;
+  });
+}
+
+alphabetKeys.addEventListener('click', (e) => {
+  const wordletter = document.querySelectorAll('.word-letter');
+  wordletter.forEach(letterhtml => {
+    if (e.target.id === letterhtml.dataset.value) {
+      console.log("YES!");
+      letterhtml.innerHTML = e.target.id;
+    }
+  })
+})
+
+secretWord("escape");
+buildAlphabet(alphabet);
