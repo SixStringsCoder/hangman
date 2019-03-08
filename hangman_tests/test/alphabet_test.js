35d00b98218dda41f5a220436f6a94b4d057d5ec
steve hanlon <rshanlon@gmail.com>
2019-03-07 16:51:59 -0800

    Refactor code into Model-Manager-View; add ledge graphic; add fingers HTML and CSS

--- hangman_tests/test/alphabet_test.js
+++ hangman_tests/test/alphabet_test.js
@@ -2,10 +2,18 @@ var expect = require('chai').expect;
 const jsdom = require('jsdom-global')();
 
 describe('buildAlphabet', () => {
-  var buildAlphabet = require('../../script').buildAlphabet;
+  const buildAlphabet = require('../../script').buildAlphabet;
   jsdom();
   it('should accept an array as an argument', () => {
-    const arr = ["A", "E", "I", "O", "U"];
-    expect(buildAlphabet(arr)).to.be.true
+    arr = ["A", "E"];
+    expect(buildAlphabet(arr)).to.be.an('array');
   })
 });
+
+describe('secretWord', () => {
+  const secretWord = require('../../script').secretWord;
+  jsdom();
+  it('should accept a string as an argument', () => {
+    expect(secretWord("today")).to.be.a('string');
+  })
+})
--- script.js
+++ script.js
@@ -1,36 +1,68 @@
-const alphabetKeys = document.querySelector('#alphabet-keyboard');
-const letterblanks = document.querySelector('#letterspaces');
-const alphabet =   ["A", "E", "I", "O", "U", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"];
+// MODEL never communicates with VIEW, only with MANAGER
+const model = {
+  alphabet:   ["A", "E", "I", "O", "U", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"],
+  challenges: ["escape", "Hollywood", "empire", "trident", "frisbee"],
+};
 
 
+// VIEW never communicates with MODEL, only with MANAGER
+const view = {
+  init: function() {
+    this.alphabetKeys = document.querySelector('#alphabet-keyboard');
+    this.letterblanks = document.querySelector('#letterspaces');
+    this.ledge = document.querySelector('#top-of-ledge');
+    this.renderAlphabet();
+    this.renderChallenge();
+    this.fingers = 8;
+    this.renderFingers();
+    this.alphabetKeys.addEventListener('click', this.renderLetters);
+  },
 
-const buildAlphabet = (arr) => {
-  // arr.map((letter, index) => {
-  //   let htmlpattern = `<div class="letter" id="${letter}">${letter}</div>`;
-  //   alphabetKeys.innerHTML += htmlpattern;
-  // });
-}
+  // Builds Alphabet keysboard
+  renderAlphabet: function() {
+    const arr = manager.getAlphabet();
+    arr.map((letter, index) => {
+      let htmlpattern = `<div class="letter" id="${letter}">${letter}</div>`;
+      this.alphabetKeys.innerHTML += htmlpattern;
+    });
+  },
 
-// const secretWord = (word) => {
-//   let wordArr = word.toUpperCase().split('');
-//   console.log(wordArr);
-//   wordArr.map((letter) => {
-//     let blank = `<div class="word-letter" data-value="${letter}"></div>`;
-//     letterblanks.innerHTML += blank;
-//   });
-// }
-//
-// alphabetKeys.addEventListener('click', (e) => {
-//   const wordletter = document.querySelectorAll('.word-letter');
-//   wordletter.forEach(letterhtml => {
-//     if (e.target.id === letterhtml.dataset.value) {
-//       console.log("YES!");
-//       letterhtml.innerHTML = e.target.id;
-//     }
-//   })
-// })
+   // Builds blank spaces for challenge word
+  renderChallenge: function() {
+    let word = manager.getChallenge();
+    let wordArr = word.toUpperCase().split('');
+    wordArr.map((letter) => {
+      let blank = `<div class="word-letter" data-value="${letter}"></div>`;
+      this.letterblanks.innerHTML += blank;
+    });
+  },
 
-// secretWord("escape");
-buildAlphabet(alphabet);
+  renderLetters: function(e) {
+    this.wordletter = document.querySelectorAll('.word-letter');
+    this.wordletter.forEach(letterDiv => {
+      if (e.target.id === letterDiv.dataset.value) {
+        letterDiv.innerHTML = e.target.id;
+      }
+    });
+  },
 
-module.exports.buildAlphabet = buildAlphabet;
+  renderFingers: function() {
+
+    for (let i = 0; i < this.fingers; i += 1) {
+       let fingerHTML = `<div class="finger" id="finger${i}"><div class="fingernail"></div></div>`;
+      this.ledge.innerHTML += fingerHTML;
+    }
+  },
+};
+
+// MANAGER - always sits between MODEL and VIEW and delegates info and tasks
+const manager = {
+  init: () => view.init(),
+
+  getAlphabet: () => model.alphabet,
+
+  getChallenge: () => model.challenges[Math.floor(Math.random() * model.challenges.length)],
+};
+
+
+manager.init();
